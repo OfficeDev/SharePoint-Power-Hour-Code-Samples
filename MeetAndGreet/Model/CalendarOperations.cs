4a26c04690a57388d545ef77179ca73ec84bf9a0
Andrew Connell <me@andrewconnell.com>
2014-12-08 10:03:27 -0500

    updated MeetAndGreet Win81 app to (1) use O365 RTM API and SDK, (2) follow auth pattern from other Win81 apps in OfficeDev, (3) use repository model for accessing data endpoints in API

---
+++ MeetAndGreet/Model/CalendarOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office365.OutlookServices;
+using MeetAndGreet.Helpers;
+
+namespace MeetAndGreet.Model {
+  public static class CalendarOperations {
+
+    public static async Task<IEnumerable<IEvent>> GetCalendarEvents()
+    {
+      var client = await AuthenticationHelper.EnsureOutlookClientCreatedAsync();
+
+      // Obtain calendar event data
+      var eventsResults = await (from i in client.Me.Events
+                                 where i.End >= DateTimeOffset.UtcNow
+                                 select i).ExecuteAsync();
+
+      return eventsResults.CurrentPage;
+    }
+  
+  }
+}
---
+++ MeetAndGreet/Model/ContactsOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office365.OutlookServices;
+using MeetAndGreet.Helpers;
+
+namespace MeetAndGreet.Model {
+  public static class ContactsOperations {
+    public static async Task<IEnumerable<IContact>> GetContacts() {
+      var client = await AuthenticationHelper.EnsureOutlookClientCreatedAsync();
+
+      // Obtain first page of contacts
+      var contactsResults = await client.Me.Contacts.Select(c => c).ExecuteAsync();
+
+      return contactsResults.CurrentPage;
+    }
+
+  }
+}
